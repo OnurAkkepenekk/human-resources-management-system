51a04ad7c6275ba55eaab4c63f7a70c075330cff
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-11 02:47:37 +0300

    Create employee class

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employee.java
@@ -0,0 +1,28 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import kodlamaio.hrms.entities.abstracts.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name ="employees")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee extends User{
+
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+}
