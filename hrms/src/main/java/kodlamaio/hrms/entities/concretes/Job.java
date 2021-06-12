80eff29f0c5fdcbe51a4185dd9f6e9579e35645f
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-12 23:20:52 +0300

    Update job class

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
@@ -1,10 +1,13 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -21,5 +24,8 @@ public class Job {
 	private int id;
 
 	@Column(name = "job_title")
-	private String job_title;	
+	private String job_title;
+
+	@OneToMany(mappedBy = "jobPosition")
+	private List<JobAdvertisement> jobAdvertisements;
 }
