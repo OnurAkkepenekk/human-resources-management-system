f64b84d9a4125b840ed274304fc62f76dab8433c
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-14 21:57:22 +0300

    Create job advertisement service

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobAdvertisementService.java
@@ -0,0 +1,14 @@
+package kodlamaio.hrms.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.JobAdvertisement;
+
+public interface JobAdvertisementService {
+
+	Result add(JobAdvertisement jobAdvert);
+
+	DataResult<List<JobAdvertisement>> getAll();
+}
