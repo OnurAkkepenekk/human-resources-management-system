70f596a2da35b2ff4e863d8757661a46625cfc76
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-14 21:56:54 +0300

    Create job advertisement controller

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobAdvertisementController.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobAdvertisementService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.JobAdvertisement;
+
+@RestController
+@RequestMapping("/api/jobadvertisement")
+public class JobAdvertisementController {
+	JobAdvertisementService advertisementService;
+
+	@Autowired
+	public JobAdvertisementController(JobAdvertisementService advertisementService) {
+		super();
+		this.advertisementService = advertisementService;
+	}
+
+	@PostMapping("/add")
+	public Result Add(@RequestBody JobAdvertisement jobAdvert) {
+		return this.advertisementService.add(jobAdvert);
+	}
+	
+	@GetMapping("/getAll")
+	public DataResult<List<JobAdvertisement>> getAll(){		
+		return this.advertisementService.getAll();
+	}
+
+}
