a299ba7a1e8993f199375d0d3d8d7414aba9b16a
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-23 10:23:13 +0300

    Create CV entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/CV.java
@@ -0,0 +1,37 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "cvs")
+@AllArgsConstructor
+@NoArgsConstructor
+public class CV {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@OneToMany(mappedBy = "cv")
+	private List<EducationInformation> educationInformations;
+
+	@ManyToOne()
+	@JoinColumn(name = "candidate_id")
+	private Candidate candidate;
+
+}
