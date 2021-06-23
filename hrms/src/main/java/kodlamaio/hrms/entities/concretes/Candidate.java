78fdb17c6b9ea61b50c4bbd5d29004b00a608ccb
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-23 10:23:01 +0300

    Update Candidate.java

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -2,7 +2,11 @@ package kodlamaio.hrms.entities.concretes;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import kodlamaio.hrms.entities.abstracts.User;
 import lombok.AllArgsConstructor;
@@ -11,6 +15,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -18,6 +23,7 @@ import java.util.Date;
 @Table(name = "candidates")
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","cvs"})
 public class Candidate extends User {
 
 	@Column(name = "first_name")
@@ -31,4 +37,8 @@ public class Candidate extends User {
 
 	@Column(name = "birth_date")
 	private Date birth_date;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "candidate")
+	private List<CV> cvs;
 }
