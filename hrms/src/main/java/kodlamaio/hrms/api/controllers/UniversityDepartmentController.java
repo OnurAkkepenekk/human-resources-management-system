e87ad55762c7a8f50d305e2f8e1f97a4f9662232
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-23 09:16:25 +0300

    Add UniversityDepartment manager, service controller

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/UniversityDepartmentController.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.UniversityDepartmentService;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.UniversityDepartment;
+
+@RestController
+@RequestMapping("/api/universitydepartments")
+public class UniversityDepartmentController {
+
+	UniversityDepartmentService universityDepartmentService;
+
+	@Autowired
+	public UniversityDepartmentController(UniversityDepartmentService universityDepartmentService) {
+		super();
+		this.universityDepartmentService = universityDepartmentService;
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody UniversityDepartment universityDepartment) {
+		return this.universityDepartmentService.add(universityDepartment);
+
+	}
+}
