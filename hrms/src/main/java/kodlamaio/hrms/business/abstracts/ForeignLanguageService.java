cefc605f0996eb67e55317496378a9f60d263f73
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-24 10:13:31 +0300

    Create foreign language service

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/ForeignLanguageService.java
@@ -0,0 +1,9 @@
+package kodlamaio.hrms.business.abstracts;
+
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.ForeignLanguage;
+
+public interface ForeignLanguageService {
+
+	public Result add(ForeignLanguage foreignLanguage);
+}
