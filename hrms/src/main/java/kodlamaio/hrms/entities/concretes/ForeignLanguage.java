b9ee8a505c1a2159c9f2b005cdf4e9eeb332083f
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-24 10:13:02 +0300

    Create foreign language entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/ForeignLanguage.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "foreign_languages")
+public class ForeignLanguage {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(hidden = true)
+	private int id;
+
+	@Column(name = "language_name")
+	private String language;
+
+	@Column(name = "language_level")
+	private int languageLevel;
+
+	@ManyToOne()
+	@JoinColumn(name = "cv_id")
+	private CV cv;
+}
