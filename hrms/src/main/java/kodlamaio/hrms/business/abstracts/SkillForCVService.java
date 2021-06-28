03dfa953a2c207b6f9c977df4ce0ea3f9967e037
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-28 11:07:25 +0300

    Create SkillForCV service

---
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/SkillForCVService.java
@@ -0,0 +1,9 @@
+package kodlamaio.hrms.business.abstracts;
+
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.SkillForCV;
+
+public interface SkillForCVService {
+
+	Result add(SkillForCV skillForCD);
+}
