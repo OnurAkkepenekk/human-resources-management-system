36a9dc7c7cf34fea60732fab20eae118e6ba8333
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-28 11:06:38 +0300

    Create SkillForCV entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/SkillForCV.java
@@ -0,0 +1,38 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "skills_for_CV")
+public class SkillForCV {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "skill_id")
+	private Skill skill;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	@ManyToOne
+	@JoinColumn(name = "cv_id")
+	private CV cv;
+}
