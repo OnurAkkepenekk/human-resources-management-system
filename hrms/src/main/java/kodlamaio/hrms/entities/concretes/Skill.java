65fa5c46258f4dfbd0cee67ed94cb3dd98808f94
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-28 11:02:25 +0300

    Create skill entity

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Skill.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="skills")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","skillForCV"})
+public class Skill {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="skill_name")
+	private String skillName;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="skill")
+	private List<SkillForCV> skillForCV;
+}
