7e63589cd0c372ca686cc9477024222f77740cc4
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-28 11:04:03 +0300

    Create skill manager

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SkillController.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.SkillService;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.entities.concretes.Skill;
+
+@RestController
+@RequestMapping("/api/skill")
+public class SkillController {
+
+	private SkillService skillService;
+
+	@Autowired
+	public SkillController(SkillService skillService) {
+		super();
+		this.skillService = skillService;
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody Skill skill) {
+		return this.skillService.add(skill);
+	}
+}
