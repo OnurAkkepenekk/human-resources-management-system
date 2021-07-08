d7478ece1f250214b530168d6a3a350a5ebb12b1
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-08 21:04:24 +0300

    Create candidate service,manager and controller

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/CandidateManager.java
@@ -0,0 +1,40 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kodlamaio.hrms.business.abstracts.CandidateService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.ErrorDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.dataAccess.abstracts.CandidateDao;
+import kodlamaio.hrms.entities.concretes.Candidate;
+
+@Service
+public class CandidateManager implements CandidateService {
+
+	CandidateDao candidateDao;
+
+	@Autowired
+	public CandidateManager(CandidateDao candidateDao) {
+		super();
+		this.candidateDao = candidateDao;
+	}
+
+	@Override
+	public DataResult<List<Candidate>> getAll() {
+		return new SuccessDataResult<List<Candidate>>(this.candidateDao.findAll());
+	}
+
+	@Override
+	public DataResult<Candidate> getById(@RequestParam int id) {
+		if (this.candidateDao.existsById(id)) {
+			return new SuccessDataResult<Candidate>(this.candidateDao.findById(id));
+		}
+		return new ErrorDataResult<>("User not found");
+	}
+
+}
