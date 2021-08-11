52430a1d8ffb644aaf47131ac2f1cc0996a000a3
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-08-11 18:26:52 +0300

    add a new method to get employee by id

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EmployeeDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EmployeeDao.java
@@ -1,9 +1,12 @@
 package kodlamaio.hrms.dataAccess.abstracts;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import kodlamaio.hrms.entities.concretes.Employee;
 
 public interface EmployeeDao extends JpaRepository<Employee, Integer> {
 
+	@Query("From Employee where id=:id")
+	Employee getById(int id);
 }
