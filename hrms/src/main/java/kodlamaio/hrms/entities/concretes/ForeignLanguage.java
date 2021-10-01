401d74c6e933533336e1e1f437245a72726345a0
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-01 12:00:13 +0300

    Update ForeignLanguage.java

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/ForeignLanguage.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/ForeignLanguage.java
@@ -9,7 +9,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
