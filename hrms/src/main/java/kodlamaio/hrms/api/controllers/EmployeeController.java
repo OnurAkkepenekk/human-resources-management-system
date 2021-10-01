ebaa48bde20ef7939dc5b501840e83847b4af7d7
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-01 11:52:29 +0300

    Add employee add method

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/EmployeeController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/EmployeeController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.EmployeeService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.entities.concretes.Employee;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +28,10 @@ public class EmployeeController {
 	@GetMapping("/getAll")
 	public DataResult<List<Employee>>getAll(){
 		return this.employeeService.getAll();
+	}
+	@PostMapping("/add")
+	public Result add(@RequestBody Employee employee){
+		 
+		 return this.employeeService.add(employee);
 	}
 }
