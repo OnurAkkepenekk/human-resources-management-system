2ae12cbe5f889c2b7d82359642f5d3701b759086
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-01 12:00:09 +0300

    Update WorkTimeType.java

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/WorkTimeType.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/WorkTimeType.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
@@ -32,5 +33,6 @@ public class WorkTimeType {
 	private String workTimeTypeName;
 	
 	@OneToMany(mappedBy = "workTimeType")
+	@JsonIgnore
 	private List<JobAdvertisement> jobAdvertisement;
 }
