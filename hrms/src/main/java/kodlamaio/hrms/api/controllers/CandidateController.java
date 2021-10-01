2c63307c02454d2543b043acbdcfb739e8f2792c
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-01 11:51:28 +0300

    Add candidate add method
    Add candidate add method

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidateController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CandidateController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.CandidateService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Candidate;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -29,5 +32,10 @@ public class CandidateController {
 	@GetMapping("/findbyid")
 	public DataResult<Candidate>findById(int id){
 		return this.candidateService.getById(id); 
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Candidate candidate){
+		return this.candidateService.add(candidate); 
 	}
 }
