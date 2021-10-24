fd52993dc08e0ef30fbbf3a4e0ffe123f3059c72
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-24 17:11:27 +0300

    add a new updateCvCreationDate method

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/CVController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/CVController.java
@@ -2,8 +2,12 @@ package kodlamaio.hrms.api.controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +21,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/cvs")
 @RequiredArgsConstructor
 public class CVController {
@@ -27,11 +32,17 @@ public class CVController {
 	@PostMapping("/add")
 	public Result add(@RequestBody CV cv) {
 		return this.cvService.add(cv);
-
 	}
 
 	@GetMapping("/getcv")
 	public DataResult<List<CV>> getCv(@RequestParam int candidateId) {
 		return this.cvService.getCV(candidateId);
+	}
+	
+	@Transactional
+	@PutMapping("/updateCvCreationDate")
+	public Result updateCvCreationDate(@RequestParam int id) {
+		return this.cvService.updateCreationDate(id);
+		
 	}
 }
