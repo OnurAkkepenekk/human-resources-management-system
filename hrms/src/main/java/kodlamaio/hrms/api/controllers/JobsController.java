23017308ded8ca82d2ef620bd8ede310ef6f717e
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-30 13:14:45 +0300

    bugfix

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.hrms.business.abstracts.JobService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.entities.concretes.Job;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
