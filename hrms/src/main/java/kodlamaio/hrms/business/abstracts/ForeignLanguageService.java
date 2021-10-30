2a6e7e3a119e1d9229f937b5a89208a64f923695
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-10-30 13:03:37 +0300

    Add foreign language components

--- hrms/src/main/java/kodlamaio/hrms/business/abstracts/ForeignLanguageService.java
+++ hrms/src/main/java/kodlamaio/hrms/business/abstracts/ForeignLanguageService.java
@@ -1,9 +1,15 @@
 package kodlamaio.hrms.business.abstracts;
 
+import java.util.List;
+
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.ForeignLanguage;
+import kodlamaio.hrms.entities.dtos.ForeignLanguageDtoCv;
 
 public interface ForeignLanguageService {
 
 	public Result add(ForeignLanguage foreignLanguage);
+
+	public DataResult<List<ForeignLanguageDtoCv>> getForeignLanguageListByCvId(int cvId);
 }
