a1ac4f98480b444936a63b5f41a225ac5e97434c
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-11-07 00:12:35 +0300

    Create a new method to get links by cvId
    * Update link service,manager and controller.
    * Create LinkDto

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/LinkController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/LinkController.java
@@ -1,19 +1,27 @@
 package kodlamaio.hrms.api.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.LinkService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Link;
+import kodlamaio.hrms.entities.dtos.LinkDto;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/links")
 @RequiredArgsConstructor
+@CrossOrigin
 public class LinkController {
 	@NonNull
 	private LinkService linkService;
@@ -21,6 +29,11 @@ public class LinkController {
 	@PostMapping("/add")
 	public Result add(@RequestBody Link link) {
 		return this.linkService.add(link);
+	}
+	
+	@GetMapping("/getLinksByCvId")
+	public DataResult<List<LinkDto>> getLinksByCvId(@RequestParam int cvId) {
+		return this.linkService.getLinksByCvId(cvId);
 	}
 
 }
