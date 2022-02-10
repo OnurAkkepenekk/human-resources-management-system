6d1238ab47e32d5b050bed90e1b63e1895024757
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-02-10 22:39:37 +0300

    Update HrmsApplication.java

--- hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -19,9 +22,17 @@ public class HrmsApplication {
 	
 	@Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("kodlamaio.hrms"))                                     
-          .build();                                           
+        return new Docket(DocumentationType.SWAGGER_2)
+        		.apiInfo(apiEndPointsInfo())
+        		.select()                                  
+        		.apis(RequestHandlerSelectors.basePackage("kodlamaio.hrms"))                                     
+        		.build();                                           
     }
+	   private ApiInfo apiEndPointsInfo() {
+	        return new ApiInfoBuilder().title("Human Resources Management System REST API Layer")
+	                .description("Human Resources Management")
+	                .contact(new Contact("Onur Akkepenek", "https://github.com/OnurAkkepenekk", "onurakkepenek@outlook.com"))
+	                .version("1.0")
+	                .build();
+	    }
 }
