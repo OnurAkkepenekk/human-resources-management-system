98f84ef464e4b3353afee21be15683ac7a65dbea
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-03-15 00:42:31 +0300

    Update SecurityConfig.java

--- hrms/src/main/java/kodlamaio/hrms/security/SecurityConfig.java
+++ hrms/src/main/java/kodlamaio/hrms/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package kodlamaio.hrms.security;
 
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import kodlamaio.hrms.security.filter.CustomAuthenticationFilter;
 import kodlamaio.hrms.security.filter.CustomAuthorizationFilter;
@@ -32,37 +36,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().authorizeRequests().antMatchers("/resource").permitAll();
 		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
 				authenticationManagerBean());
 		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+		// Disable CSRF (cross site request forgery)
 		http.csrf().disable();
+
+		// No session will be created or used by spring security
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		// Entry points
 		http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
 		http.authorizeRequests().antMatchers("GET", "/api/user/**").hasAnyAuthority("ROLE_USER");
 		http.authorizeRequests().antMatchers("POST", "/api/user/save/**").permitAll();
 		http.authorizeRequests().anyRequest().permitAll();
-		http.formLogin();
 		http.addFilter(customAuthenticationFilter);
 		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+
 	@Override
-    public void configure(WebSecurity web) throws Exception {
-        // Allow swagger to be accessed without authentication
-        web.ignoring().antMatchers("/v2/api-docs")//
-                .antMatchers("/swagger-resources/**")//
-                .antMatchers("/swagger-ui.html")//
-                .antMatchers("/swagger-ui/**")
-                .antMatchers("/configuration/**")//
-                .antMatchers("/webjars/**")//
-                .antMatchers("/swagger-ui/index.html#/")
-                .antMatchers("/public")
+	public void configure(WebSecurity web) throws Exception {
+		// Allow swagger to be accessed without authentication
+		web.ignoring().antMatchers("/v2/api-docs")//
+				.antMatchers("/swagger-resources/**")//
+				.antMatchers("/swagger-ui.html")//
+				.antMatchers("/swagger-ui/**").antMatchers("/configuration/**")//
+				.antMatchers("/webjars/**")//
+				.antMatchers("/swagger-ui/index.html#/").antMatchers("/public")
 
-                // Un-secure H2 Database (for testing purposes, H2 console shouldn't be unprotected in production)
-                .and()
-                .ignoring()
-                .antMatchers("/h2-console/**/**");
+				// Un-secure H2 Database (for testing purposes, H2 console shouldn't be
+				// unprotected in production)
+				.and().ignoring().antMatchers("/h2-console/**/**");
 
-    }
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
