c32cc4fd52945e404bd453bfafc30d3e3d972a2a
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-03-15 00:45:34 +0300

    Update HrmsApplication.java

--- hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -8,11 +8,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -21,7 +21,7 @@ public class HrmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
-
+	
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
